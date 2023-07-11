99803b163d1cf33481e192fe0744b7e294f6e27d
Bruno Silva <brunosilva.dev@gmail.com>
2023-07-10 23:53:09 -0500

    feat: full CRUD operations

--- Hon.App/Persistence/DbService/HonDatabaseService.cs
+++ Hon.App/Persistence/DbService/HonDatabaseService.cs
@@ -13,14 +13,41 @@ public class HonDatabaseService : IDatabaseService
     {
         return await _context.Samples.ToListAsync();
     }
+    public async Task<SampleModel?> GetSample(int sampleId)
+    {
+        return await _context.Samples.FindAsync(sampleId);
+    }
     public async Task AddSample(SampleModel sample)
     {
-        throw new NotImplementedException();            
+        _context.Samples.Add(sample);
+        await _context.SaveChangesAsync();          
+    }
+    public async Task UpdateSample(SampleModel sample)
+    {
+        var sampleToUpdate = await _context.Samples.FindAsync(sample.SampleId);
+        if (sampleToUpdate != null)
+        {
+            sample.SampleName = sample.SampleName;
+            _context.Samples.Update(sample);
+            await _context.SaveChangesAsync();
+        }
+    }
+    public async Task DeleteSample(int sampleId)
+    {
+        var sample = await _context.Samples.FindAsync(sampleId);
+        if (sample != null)
+        {
+            _context.Samples.Remove(sample);
+            await _context.SaveChangesAsync();
+        }
     }
 }
 
 public interface IDatabaseService
 {
     Task<List<SampleModel>> GetSamples();
+    Task<SampleModel> GetSample(int sampleId);
     Task AddSample(SampleModel sample);
+    Task UpdateSample(SampleModel sample);
+    Task DeleteSample(int sampleId);
 }
--- Hon.App/Program.cs
+++ Hon.App/Program.cs
@@ -1,4 +1,5 @@
 using Hon.Persistence;
+using Hon.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDIServices();
@@ -7,12 +8,33 @@ var app = builder.Build();
 
 app.UseOpenApi();
 
-app.MapGet("/test", () => "Hello World!");
-
-app.MapGet("/testdb", async (IDatabaseService dbService) => 
+app.MapGet("/list", async (IDatabaseService dbService) => 
 {
-    await dbService.GetSamples();
+    return Results.Ok(await dbService.GetSamples());
+
 });
 
+app.MapGet("/get{id}", async (int id, IDatabaseService dbService) =>
+{
+    return Results.Ok(await dbService.GetSample(id));
+});
+
+app.MapPost("/create", async (SampleModel sample, IDatabaseService dbService) =>
+{
+    await dbService.AddSample(sample);
+    return Results.CreatedAtRoute($"/create/{sample.SampleId}",sample);
+});
+
+app.MapPut("/update", async (SampleModel sample, IDatabaseService dbService) =>
+{
+    await dbService.UpdateSample(sample);
+    return Results.Ok(sample);
+});
+
+app.MapDelete("/delte{id}", async (int id, IDatabaseService dbService) =>
+{
+    await dbService.DeleteSample(id);
+    return Results.Ok();
+});
 
 app.Run();
